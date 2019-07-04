b48000e49f968f905cc9d2a46bc20a967cf527e1
ryuna05102 <ryuna05102@yahoo.co.jp>
2019-07-04 12:50:21 +0900

    検索ワードにてでたレシピページからレシピ情報をscraping

--- scrap_resipi.py
+++ scrap_resipi.py
@@ -3,50 +3,53 @@
 import requests
 import pandas as ps
 import csv
+import time
 from bs4 import BeautifulSoup as bs4
 
-def scaping(URL):
+def scraping(urls):
         #scraping url
         Date=[]
-        url = "http://cookpad.com"+URL
-        response = requests.get(url)
-        #
-        response.encoding = response.apparent_encoding 
-        bs = bs4(response.content,'lxml')
-        #
-        #
-        someRecipe = bs.find_all("div",attrs={'id':'recipe'})
-        for someRecipes in someRecipe:
-                Date.extend([
-                        someRecipes.select('#recipe-title')[0].text,
-                        someRecipes.select('.description_text')[0].text,
-                        someRecipes.select('#ingredients_list')[0].text,
-                        someRecipes.select('#steps')[0].text,
-                        someRecipes.select('#advice')[0].text
-                ])
-        txt = [rmTag(Date),''] 
+        space = "\n"
+        for search in urls:
+                url = "http://cookpad.com"+search
+                response = requests.get(url)
+                #
+                response.encoding = response.apparent_encoding 
+                bs = bs4(response.content,'lxml')
+                #
+                #
+                someRecipe = bs.find_all("div",attrs={'id':'recipe'})
+                for someRecipes in someRecipe:
+                        Date.extend([
+                                someRecipes.select('#recipe-title')[0].text,
+                                someRecipes.select('.description_text')[0].text,
+                                someRecipes.select('#ingredients_list')[0].text,
+                                someRecipes.select('#steps')[0].text,
+                                someRecipes.select('#advice')[0].text
+                        ])
+                        time.sleep(1)
+                        txt = [rmTag(Date)+space,''] 
+                        with open('test9.csv','a') as f:
+                                writer = csv.writer(f,lineterminator='\n')
+                                writer.writerow(txt)
+
 
         
-        with open('test7.csv','a') as f:
-                writer = csv.writer(f,lineterminator='\n')
-                writer.writerow(txt)
-
-
 def getRecipesURL():
         url = 'https://cookpad.com/search/'
         urls = []
         for words in searchWords:
                 response = requests.get(url+words)
-
                 response.encoding = response.apparent_encoding 
                 bs = bs4(response.content,'lxml')
 
-
                 someURLs = bs.find_all("a",attrs={"class":"recipe-title font13"})
-                i = 0
+                # i = 0
                 for u in someURLs:
-                        urls[i] = u.get('href')
-
+                        
+                        urls.append(u.get('href'))
+                        # print(urls[i])
+                        # i+=
         return urls
 
 #remove tag 
@@ -55,18 +58,18 @@ def rmTag(someRecipe):
         for line in someRecipe:
                 line.strip() ##\n --> indention
                 lines = line.splitlines()#indention --> '' 
-                text+="\n".join(line for line in lines if line)+"\n" #remove ''
+                text+="\n".join(line for line in lines if line) #remove ''
         return text
-
-        
+       
 if __name__ == "__main__":
         searchWords = [
-
                 '鶏肉'#,
                 # '豚肉',
                 # '牛肉',
                 # 'パスタ',
                 # 'うどん',
         ]
-        #URLS = getRecipesURL()
-        scaping("/recipe/5712000")
+        URLS = getRecipesURL()
+        scraping(URLS)
+        
+        # scraping("/recipe/5712000")
